ec07107d8b65d8c42719b9ca2f8ba3f954fb51c7
ashashiddo <53973974+ashashiddo@users.noreply.github.com>
2023-06-08 08:34:47 -0400

    stdin setup

--- play.js
+++ play.js
@@ -1,4 +1,25 @@
 const { connect } = require("./client");
 
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    return stdin;
+  };
+
+  const handleUserInput = function (key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+  
 console.log("Connecting ...");
 connect();
+
+const stdin = setupInput();
+
+stdin.on("data", (key) => {
+    handleUserInput(key);
+  });
+  
